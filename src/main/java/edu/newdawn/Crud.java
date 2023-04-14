5c63c9e03de501f93c7f596f13fdf9a06f129e2e
SPiedra955 <spiedramite@cifpfbmoll.eu>
2023-04-14 23:44:42 +0200

    updated files

--- src/main/java/edu/newdawn/Crud.java
+++ src/main/java/edu/newdawn/Crud.java
@@ -13,31 +13,59 @@ public class Crud {
     public static void main (String[] args) throws SQLException{
     try{
         initDatabaseConnection();
-        deleteData("%");
+        dropTable();
+        createTable();
         readData();
-        createData("Java", 10);
-        createData("JavaScript", 9);
-        createData("C++", 8);
-        createData("Html", 9);
+        createData("Justin", "Perez", "Oms", 7, true);
+        createData("Joan", "Marti", "Marti", 1, false);
+        createData("Luis", "Garcia", "Paz", 5, true);
+        createData("Andres", "Naranjo", "Valdez", 2, false);
+        createData("Anabel", "Rigo", "Font", 2, false);
         readData();
-        updateData("C++", 5);
-        updateData("Java", 5);
+        updateData("Joan", "Marti", 5);
+        updateData("Andres","Naranjo", 5);
         readData();
-        deleteData("Html");
+        deleteData("Rigo", "Font");
         readData();
         } finally{
             closeDatabaseConnection();
         }
     }
 
-    /* INSERTING DATA (create table before run the file) */
-    private static void createData(String name, int rating) throws SQLException{
+   private static void dropTable() throws SQLException {
+    try (PreparedStatement statement = connection.prepareStatement("DROP TABLE IF EXISTS db_course")) {
+        statement.executeUpdate();
+        System.out.println("Deleting records...");
+    }
+}
+
+    private static void createTable() throws SQLException {
+        try (PreparedStatement statement = connection.prepareStatement("""
+                CREATE TABLE db_course (
+                    name VARCHAR(50) NOT NULL,
+                    surname VARCHAR(50) NOT NULL,
+                    last_name VARCHAR(50) NOT NULL,
+                    score INT NOT NULL,
+                    approved TINYINT NOT NULL
+                )
+                """)) {
+            statement.executeUpdate();
+            System.out.println("Creating table...");
+        }
+    }
+
+    /* INSERTING DATA (create table and database before run the file) */
+
+    private static void createData(String name, String surname, String last_name, int score, Boolean approved) throws SQLException{
         try(PreparedStatement statement = connection.prepareStatement("""
-                    INSERT INTO programming_language(name, rating)
-                    VALUES(?, ?)
+                    INSERT INTO db_course(name, surname, last_name, score, approved)
+                    VALUES(?, ?, ?, ?, ?)
                     """)) {
             statement.setString(1, name); /* inserting values by index of the parameters */
-            statement.setInt(2, rating);
+            statement.setString(2, surname);
+            statement.setString(3, last_name);
+            statement.setInt(4, score);
+            statement.setBoolean(5, approved);
             int rowsInserted = statement.executeUpdate();
             System.out.println("Creating data...");
             System.out.println("Rows inserted: " + rowsInserted);
@@ -49,9 +77,9 @@ public class Crud {
 
     private static void readData() throws SQLException{
         try(PreparedStatement statement =connection.prepareStatement("""
-                    SELECT name, rating
-                    FROM programming_language
-                    ORDER by rating DESC
+                    SELECT name, surname, score, approved
+                    FROM db_course
+                    ORDER by score DESC
                     """)) {
         try(ResultSet resultSet = statement.executeQuery()){
                         boolean empty = true;
@@ -59,8 +87,12 @@ public class Crud {
                         while(resultSet.next()){
                             empty = false;
                             String name = resultSet.getString("name"); /* reading data by the name of the column */
-                            int rating = resultSet.getInt("rating");
-                            System.out.println("\t> " + name + ": " + rating);
+                            String surname = resultSet.getString("surname");
+                            int score = resultSet.getInt("score");
+                            Boolean approved = resultSet.getBoolean("approved");
+                            System.out.println("\t> " + name + " " + surname + " = " + 
+                                               " aprobado -> " + approved
+                                               + "(" + score + ")");
                         }
                         if(empty){
                             System.out.println("Reading data...");
@@ -72,14 +104,16 @@ public class Crud {
     
     /* UPDATE DATA */
 
-    private static void updateData(String name, int newRating) throws SQLException{
+    private static void updateData(String surname, String last_name, int newscore) throws SQLException{
         try(PreparedStatement statement = connection.prepareStatement("""
-                    UPDATE programming_language
-                    SET rating = ?
-                    WHERE name = ?
+                    UPDATE db_course
+                    SET score = ?
+                    WHERE surname = ?
+                    AND last_name = ?
                     """)) {
-            statement.setInt(1, newRating);
-            statement.setString(2, name);
+            statement.setInt(1, newscore);
+            statement.setString(2, surname);
+            statement.setString(3, last_name);
             int rowsUpdated = statement.executeUpdate();
             System.out.println("Updating data...");
             System.out.println("Rows update: " + rowsUpdated);                
@@ -88,12 +122,14 @@ public class Crud {
 
     /* DELETE DATA */
 
-    private static void deleteData(String nameExpression) throws SQLException{
+    private static void deleteData(String surnameExpression, String last_nameExpression) throws SQLException{
         try(PreparedStatement statement = connection.prepareStatement("""
-                    DELETE FROM programming_language
-                    WHERE name LIKE ? 
+                    DELETE FROM db_course
+                    WHERE surname LIKE ?
+                    AND last_name = ? 
                     """)){
-            statement.setString(1, nameExpression);
+            statement.setString(1, surnameExpression);
+            statement.setString(2, last_nameExpression);
             int rowsDeleted = statement.executeUpdate();
             System.out.println("Deleting data...");
             System.out.println("Rows deleted: " + rowsDeleted);
@@ -105,7 +141,7 @@ public class Crud {
     private static void initDatabaseConnection() throws SQLException{
             System.out.println("Connecting to database...");
             connection = DriverManager.getConnection(
-                "jdbc:mariadb://localhost:3306/uni", 
+                "jdbc:mariadb://localhost:3306/students", 
                 "root", "1234");
             System.out.println("Connection valid: " + connection.isValid(5));
     }
