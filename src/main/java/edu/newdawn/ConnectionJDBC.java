886a681ed93f5838a81b81fb62299e3783658011
SPiedra955 <spiedramite@cifpfbmoll.eu>
2023-04-14 19:12:27 +0200

    update files

--- src/main/java/edu/newdawn/ConnectionJDBC.java
+++ src/main/java/edu/newdawn/ConnectionJDBC.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class JdbcConnection {
+public class ConnectionJDBC {
 
 	private static Connection connection;
 
